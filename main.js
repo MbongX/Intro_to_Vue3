b15b5aa28969b51867a39b822f27b95253c393e7
Mbongeni Mahlangu <mbongenimahlangu203@outlook.com>
2025-02-21 06:14:46 +0200

    Lesson 2 Challenge Completed and ammend styles

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data(){ // <- this is a functionin the es6 syntax | origanal is data: function (){}
         return {
-            product: 'Socks - changed'
+            product: 'Socks - changed',
+            description : "This is the product's description"
         }
     }
 })
